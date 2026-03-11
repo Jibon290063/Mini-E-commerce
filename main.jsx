877cc2a0e9faba7261c584deeabdf37fe9bef404
ZIBON <jsroy5796@gmail.com>
2026-03-11 20:01:15 +0600

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,12 +1,1 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './assets/components/App'
 
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
